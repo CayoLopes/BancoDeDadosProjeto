bc7d7dc8e9712f80f26b6903cd479b9faf224743
Gustavo-DSC <gustavodouglas646@gmail.com>
2025-05-20 22:22:47 -0300

    add: procedimento de gerar relatório de faltas #18

--- database/tarefas/p02/gustavo/procedimentos_estoque.sql
+++ database/tarefas/p02/gustavo/procedimentos_estoque.sql
@@ -9,3 +9,23 @@ BEGIN
     WHERE id = prod_id;
 END;
 $$;
+
+-- Procedimento 2: Gerar relatório de faltas
+CREATE OR REPLACE PROCEDURE gerar_relatorio_faltas(
+    estoque_minimo INT
+) LANGUAGE plpgsql AS $$
+BEGIN
+    CREATE TEMP TABLE IF NOT EXISTS tmp_produtos_faltando (
+        id INT,
+        nome VARCHAR(100),
+        quantidade INT
+    );
+    
+    DELETE FROM tmp_produtos_faltando;  -- limpa antes de popular novamente
+    
+    INSERT INTO tmp_produtos_faltando
+    SELECT id, nome, quantidade
+    FROM produto
+    WHERE quantidade < estoque_minimo;
+END;
+$$;
